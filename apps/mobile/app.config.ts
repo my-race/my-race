26dd8b94a51c7a180dc4b45559fc4718e2c2fb91
Bran <june20516@gmail.com>
2025-06-03 22:30:19 +0900

    feat: eas preview설정

--- apps/mobile/app.config.ts
+++ apps/mobile/app.config.ts
@@ -1,38 +1,26 @@
-import 'dotenv/config';
-import type { ExpoConfig } from '@expo/config';
-
-const ENV = process.env.APP_ENV ?? 'development';
+import "dotenv/config";
+import type { ConfigContext, ExpoConfig } from "@expo/config";
 
 const envVariablesBase = {
-    APP_ENV: process.env.EXPO_PUBLIC_APP_ENV,
-    WEB_URL: process.env.EXPO_PUBLIC_WEB_URL,
-}
-
-const CONFIG: Record<string, ExpoConfig> = {
-  development: {
-    name: 'My race (Development)',
-    slug: 'my-race-development',
-    extra: {
-        ...envVariablesBase,
-    },
-  },
-  production: {
-    name: 'My race (Production)',
-    slug: 'my-race-production',
-    extra: {
-        ...envVariablesBase,
-    },
-  },
-  staging: {
-    name: 'My race (staging)',
-    slug: 'my-race-staging',
-    extra: {
-        ...envVariablesBase,
-    },
-  },
+  APP_ENV: process.env.EXPO_PUBLIC_APP_ENV,
+  WEB_URL: process.env.EXPO_PUBLIC_WEB_URL,
 };
 
-export default (): ExpoConfig => {
-  return CONFIG[ENV];
-};
-
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: "My Race",
+  slug: "my-race",
+  version: "1.0.0",
+  sdkVersion: "53.0.0",
+  ios: {
+    bundleIdentifier: "com.myrace.app",
+    supportsTablet: false,
+  },
+  plugins: [],
+  extra: {
+    ...envVariablesBase,
+    eas: {
+      projectId: "59cb161f-db78-4895-b0e3-af7e7b995b06",
+    },
+  },
+});
